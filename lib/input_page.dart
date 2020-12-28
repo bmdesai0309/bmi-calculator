544ec2915df202a08e27f966e59365780061d260
bmdesai0309 <bmdesai1685@gmail.com>
2020-12-28 22:05:09 +0530

    Create custom widget for container

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1E33);
@@ -13,43 +14,67 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Column(
-        children: [
-          Expanded(child: Row(
-            children: [
-              Expanded(child: ReusebleCard(color: activeCardColor,),
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+                child: Row(
+              children: [
+                Expanded(
+                  child: ReusebleCard(
+                    color: activeCardColor,
+                    cardChild: IconContent(icon: FontAwesomeIcons.mars, title: 'MALE',)
+                  ),
+                ),
+                Expanded(
+                  child: ReusebleCard(
+                    color: activeCardColor,
+                    cardChild: IconContent(icon: FontAwesomeIcons.venus, title: 'FEMALE',),
+                  ),
+                ),
+              ],
+            )),
+            Expanded(
+              child: ReusebleCard(
+                color: activeCardColor,
               ),
-              Expanded(child: ReusebleCard(color: activeCardColor,),),
-            ],
-          )),
-          Expanded(child: ReusebleCard(color: activeCardColor,),),
-          Expanded(child: Row(
-            children: [
-              Expanded(child: ReusebleCard(color: activeCardColor,),),
-              Expanded(child: ReusebleCard(color: activeCardColor,),),
-            ],
-          )),
-          Container(
-            color: bottomContainerColor,
-            margin: EdgeInsets.only(top: 10.0),
-            width: double.infinity,
-            height: bottomContainerHeight,
-          )
-        ],
-      )
-    );
+            ),
+            Expanded(
+                child: Row(
+              children: [
+                Expanded(
+                  child: ReusebleCard(
+                    color: activeCardColor,
+                  ),
+                ),
+                Expanded(
+                  child: ReusebleCard(
+                    color: activeCardColor,
+                  ),
+                ),
+              ],
+            )),
+            Container(
+              color: bottomContainerColor,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: bottomContainerHeight,
+            )
+          ],
+        ));
   }
 }
 
 class ReusebleCard extends StatelessWidget {
-  ReusebleCard({@required this.color});
+  ReusebleCard({@required this.color, this.cardChild});
   final Color color;
+  final Widget cardChild;
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         color: color,
@@ -59,4 +84,34 @@ class ReusebleCard extends StatelessWidget {
   }
 }
 
+class IconContent extends StatelessWidget {
+
+  final IconData icon;
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          title,
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Color(0xFF8D8E98),
+          ),
+        )
+
+      ],
+    );
+  }
+
+  IconContent({this.icon, this.title});
+}
 
