deeed6f166393f9f56be1cf3c1533a98e8baaa99
bmdesai0309 <bmdesai1685@gmail.com>
2020-12-28 22:31:07 +0530

    Create a saperate files for custom widgets

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'icon_content.dart';
+import 'reusable_card.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1E33);
@@ -25,13 +27,19 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ReusebleCard(
                     color: activeCardColor,
-                    cardChild: IconContent(icon: FontAwesomeIcons.mars, title: 'MALE',)
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.mars,
+                      title: 'MALE',
+                    ),
                   ),
                 ),
                 Expanded(
                   child: ReusebleCard(
                     color: activeCardColor,
-                    cardChild: IconContent(icon: FontAwesomeIcons.venus, title: 'FEMALE',),
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.venus,
+                      title: 'FEMALE',
+                    ),
                   ),
                 ),
               ],
@@ -67,51 +75,4 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-class ReusebleCard extends StatelessWidget {
-  ReusebleCard({@required this.color, this.cardChild});
-  final Color color;
-  final Widget cardChild;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-    );
-  }
-}
-
-class IconContent extends StatelessWidget {
-
-  final IconData icon;
-  final String title;
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(
-          icon,
-          size: 80.0,
-        ),
-        SizedBox(
-          height: 15.0,
-        ),
-        Text(
-          title,
-          style: TextStyle(
-            fontSize: 18.0,
-            color: Color(0xFF8D8E98),
-          ),
-        )
-
-      ],
-    );
-  }
-
-  IconContent({this.icon, this.title});
-}
 
