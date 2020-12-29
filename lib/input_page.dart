812469df34a7c760152a84740d873d3999c4e1af
bmdesai0309 <bmdesai1685@gmail.com>
2020-12-29 15:20:05 +0530

    Create contants.dart file for storing all the const variables and move to the next container.

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -2,11 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'icon_content.dart';
 import 'reusable_card.dart';
+import 'constants.dart';
 
-const bottomContainerHeight = 80.0;
-const activeCardColor = Color(0xFF1D1E33);
-const inactiveCardColor = Color(0xFF111328);
-const bottomContainerColor = Color(0xFFEB1555);
+
 
 enum Gender {
   male,
@@ -29,13 +27,14 @@ class _InputPageState extends State<InputPage> {
           title: Text('BMI CALCULATOR'),
         ),
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Expanded(
                 child: Row(
               children: [
                 Expanded(
                   child: ReusebleCard(
-                    color: selectedGender == Gender.male ? activeCardColor : inactiveCardColor,
+                    color: selectedGender == Gender.male ? kActiveCardColor : kInactiveCardColor,
                     cardChild: IconContent(
                       icon: FontAwesomeIcons.mars,
                       title: 'MALE',
@@ -49,7 +48,7 @@ class _InputPageState extends State<InputPage> {
                 ),
                 Expanded(
                   child: ReusebleCard(
-                    color: selectedGender == Gender.female ? activeCardColor : inactiveCardColor,
+                    color: selectedGender == Gender.female ? kActiveCardColor : kInactiveCardColor,
                     cardChild: IconContent(
                       icon: FontAwesomeIcons.venus,
                       title: 'FEMALE',
@@ -65,7 +64,12 @@ class _InputPageState extends State<InputPage> {
             )),
             Expanded(
               child: ReusebleCard(
-                color: inactiveCardColor,
+                color: kInactiveCardColor,
+                cardChild: Column(
+                  children: [
+                    Text('HEIGHT', style: kLabelTextStyle,),
+                  ],
+                ),
               ),
             ),
             Expanded(
@@ -73,21 +77,21 @@ class _InputPageState extends State<InputPage> {
               children: [
                 Expanded(
                   child: ReusebleCard(
-                    color: inactiveCardColor,
+                    color: kInactiveCardColor,
                   ),
                 ),
                 Expanded(
                   child: ReusebleCard(
-                    color: inactiveCardColor,
+                    color: kInactiveCardColor,
                   ),
                 ),
               ],
             )),
             Container(
-              color: bottomContainerColor,
+              color: kBottomContainerColor,
               margin: EdgeInsets.only(top: 10.0),
               width: double.infinity,
-              height: bottomContainerHeight,
+              height: kBottomContainerHeight,
             )
           ],
         ));
